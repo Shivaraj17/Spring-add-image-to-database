ce702f2bd973e8127b3fea58f4119665c0661665
Shivaraj17 <shivabalaraj11@gmail.com>
2024-12-31 23:20:02 +0530

    Here is the rigth code

--- src/main/java/spring_learning/com/backend_Project/controller/PostController.java
+++ src/main/java/spring_learning/com/backend_Project/controller/PostController.java
@@ -1,15 +1,23 @@
 package spring_learning.com.backend_Project.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import spring_learning.com.backend_Project.entity.Post;
 import spring_learning.com.backend_Project.payloads.*;
+import spring_learning.com.backend_Project.service.FileService;
 import spring_learning.com.backend_Project.service.PostService;
 import spring_learning.com.backend_Project.service.UserService;
 
 import javax.validation.Valid;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -19,7 +27,11 @@ public class PostController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private FileService fileService;
 
+    @Value("${project.image}")
+    private String path;
 
     @PostMapping("/user/{userId}/category/{categoryId}/posts")
     public ResponseEntity<PostDto> createPost(
@@ -86,5 +98,32 @@ public class PostController {
     public ResponseEntity<List<PostDto>> searchPostsByTitle(@PathVariable String title){
         List<PostDto> result =this.postService.searchPosts(title);
         return new ResponseEntity<>(result,HttpStatus.OK);
+    }
+
+    @PostMapping("/post/image/upload/{postId}")
+    public ResponseEntity<PostDto> uploadPostImage(
+            @RequestParam("image") MultipartFile image,
+            @PathVariable Integer postId
+            )throws IOException{
+
+        PostDto postDto = this.postService.getPostById(postId);
+
+        String fileName = this.fileService.uploadImage(path, image);
+
+        postDto.setImageName(fileName);
+
+        PostDto updatePost = this.postService.upadtePost(postDto, postId);
+
+        return new ResponseEntity<>(updatePost,HttpStatus.OK);
+    }
+
+    @GetMapping(value = "post/image/{imageName}",produces = MediaType.IMAGE_JPEG_VALUE)
+    public void downloadImage(
+            @PathVariable("imageName") String imageName,
+            HttpServletResponse response
+    )throws IOException{
+        InputStream resource =this.fileService.getResource(path, imageName);
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(resource, response.getOutputStream());
     }
 }
---
+++ src/main/java/spring_learning/com/backend_Project/service/FileService.java
@@ -0,0 +1,13 @@
+package spring_learning.com.backend_Project.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public interface FileService {
+    String uploadImage(String path, MultipartFile file) throws IOException;
+
+    InputStream getResource(String path, String fileName) throws FileNotFoundException;
+}
